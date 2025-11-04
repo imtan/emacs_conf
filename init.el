1ddd24c34b3c82b895f393b951b958be50cb45c6
imtan <lacrimalelacrimal@gmail.com>
2025-11-04 17:27:24 +0900

    fix for linux

--- init.el
+++ init.el
@@ -2,18 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
-;; ファイルの生成・読み込み（elcでは実行されない）
+;; ファイルの生成（elcでは実行されない）
+;; Note: Byte compilation is skipped here because packages need to be installed first
 (eval-when-compile
   (require 'org)
   (org-babel-tangle-file (expand-file-name "early-init.org" user-emacs-directory))
-  (org-babel-tangle-file (expand-file-name "config.org" user-emacs-directory))
-  (byte-compile-file (expand-file-name "early-init.el" user-emacs-directory))
-  (byte-compile-file (expand-file-name "config.el" user-emacs-directory)))
+  (org-babel-tangle-file (expand-file-name "config.org" user-emacs-directory)))
 
 ;; すぐバイトコンパイルできるように定義
 (defun byte-compile-init-file ()
+  "Byte-compile init.el file."
   (interactive)
   (byte-compile-file (expand-file-name "init.el" user-emacs-directory)))
+
+(defun byte-compile-config-files ()
+  "Byte-compile early-init.el and config.el files after packages are loaded."
+  (interactive)
+  (byte-compile-file (expand-file-name "early-init.el" user-emacs-directory))
+  (byte-compile-file (expand-file-name "config.el" user-emacs-directory)))
 
 ;; 設定の読み込み
 (add-to-list 'load-path (expand-file-name user-emacs-directory))
